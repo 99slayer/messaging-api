83d92803ff6ac22360f5af316cd2a42437526356
99slayer <21979862+99slayer@users.noreply.github.com>
2024-04-16 17:22:06 -0600

    Add chat_name and chat_owner fields

--- models/chat.js
+++ models/chat.js
@@ -3,6 +3,8 @@ const MessageSchema = require('./message');
 const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema({
+	chat_name: { type: String, required: true },
+	chat_owner: { type: Schema.Types.ObjectId, ref: 'User' },
 	start_date: { type: Date },
 	users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 	messages: [{ type: MessageSchema }],
