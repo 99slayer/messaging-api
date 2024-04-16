78ee2466d62101fdb1f93121730bc1bce56ab3f5
99slayer <21979862+99slayer@users.noreply.github.com>
2024-04-16 15:58:56 -0600

    Setup up cors and require dotenv package

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors');
 
 const apiRouter = require('./routes/api');
 const testRouter = require('./routes/test-routes');
@@ -11,6 +13,12 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+	cors({
+		origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+		credentials: true,
+	}),
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
