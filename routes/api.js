704a208dfc3e3120c5d1abbc20861e508176e71e
99slayer <21979862+99slayer@users.noreply.github.com>
2024-04-16 16:30:15 -0600

    Improve routes
    Added authentication to the relevant routes.
    
    Created the updateSwitch function to switch between chat update routes.

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 const chatController = require('../controllers/chatController');
 const messageController = require('../controllers/messageController');
+const auth = require('../auth');
+
+function updateSwitch(req, res, next) {
+	if (!req.body.update) {
+		throw new Error('Body missing field.');
+	}
+
+	if (req.body.update === 'remove') {
+		return next('route');
+	}
+
+	return next();
+}
+
+// AUTH ROUTES
+router.post('/auth/login', authController.auth_login);
+router.get('/auth/logout', auth.deleteRefreshToken);
+router.get('/auth/token', auth.refresh);
 
 // USER ROUTES
-router.get('/users', userController.user_list);
+router.get('/users', auth.verify, userController.user_list);
 router.post('/users', userController.user_create);
-router.get('/users/:userId', userController.user_detail);
-router.put('/users/:userId', userController.user_update);
-router.delete('/users/:userId', userController.user_delete);
+router.get('/users/:userId', auth.verify, userController.user_detail);
+router.put('/users/:userId', auth.verify, userController.user_update);
+router.delete('/users/:userId', auth.verify, userController.user_delete);
 
 // CHAT ROUTES
-router.get('/chats', chatController.chat_list);
-router.post('/chats', chatController.chat_create);
-router.get('/chats/:chatId', chatController.chat_detail);
-router.put('/chats/:chatId', chatController.chat_update);
-router.delete('/chat/:chatId', chatController.chat_delete);
+router.get('/chats', auth.verify, chatController.chat_list);
+router.post('/chats', auth.verify, chatController.chat_create);
+router.get('/chats/:chatId', auth.verify, chatController.chat_detail);
+router.put(
+	'/chats/:chatId',
+	auth.verify,
+	updateSwitch,
+	chatController.chat_add_users,
+);
+router.put('/chats/:chatId', auth.verify, chatController.chat_remove_user);
+router.delete('/chats/:chatId', auth.verify, chatController.chat_delete);
 
 // MESSAGE ROUTES
-router.get('/:chatId/messages', messageController.message_list);
-router.post('/:chatId/messages', messageController.message_create);
-router.put('/:chatId/messages/:messageId', messageController.message_update);
-router.delete('/:chatId/messages/:messageId', messageController.message_delete);
+router.get('/:chatId/messages', auth.verify, messageController.message_list);
+router.post(
+	'/chats/:chatId/messages',
+	auth.verify,
+	messageController.message_create,
+);
+router.put(
+	'/:chatId/messages/:messageId',
+	auth.verify,
+	messageController.message_update,
+);
+router.delete(
+	'/:chatId/messages/:messageId',
+	auth.verify,
+	messageController.message_delete,
+);
 
 module.exports = router;
