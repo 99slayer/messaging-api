af16d3a522e7bf175165c513054cd8ad6877d1cb
99slayer <21979862+99slayer@users.noreply.github.com>
2024-04-16 16:06:00 -0600

    Create util.js file
    Wrote getValidationMessages function to help get express-validator
    validation result objects into a better format for my purposes.

---
+++ util.js
@@ -0,0 +1,15 @@
+// Formats express-validators validationResult object.
+exports.getValidationMessages = function (result) {
+	const msgs = {};
+
+	for (const error of result.errors) {
+		if (Object.keys(msgs).includes(error.path)) {
+			msgs[error.path].push(error.msg);
+		} else {
+			msgs[error.path] = [];
+			msgs[error.path].push(error.msg);
+		}
+	}
+	console.log(msgs);
+	return msgs;
+};
